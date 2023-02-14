39db9b8c50a27dc87fa822b09f5a685cd67739cc
ResoDevansh <sachandevansh2000@gmail.com>
2023-02-14 23:00:54 +0530

    stuck in animation

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let pathArray = document.getElementsByClassName('path');
+let steps = document.getElementsByClassName('steps');
 let n = pathArray.length;
 let isTraversed = Array(n).fill(false);
-// console.log(pathArray);
+console.log(steps);
 document.getElementById('next').addEventListener('click', () => {
     let ptr = 0;
     for (let i = 0; i < n; i++){
-        if (isTraversed[i]) {
+        if (!isTraversed[i]) {
             ptr = i;
             break;
         }
     }
-    if (ptr == n-1) {
-        return;
-    }
+    isTraversed[ptr] = true;
     pathArray[ptr].classList.add('traverse');
+    steps[ptr + 1].classList.add('enhanced-step');
 })
